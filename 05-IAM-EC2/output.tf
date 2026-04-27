dc9a49c93aac347a933caee1e32e4c1bcca08822
Harsh Shrimali <harshshrimali319@gmail.com>
2026-04-27 08:24:25 +0000

    Created an IAM Role for EC2

---
+++ 05-IAM-EC2/output.tf
@@ -0,0 +1,11 @@
+output "role_name" {
+  value = aws_iam_role.ec2_role.name
+}
+
+output "role_arn" {
+  value = aws_iam_role.ec2_role.arn
+}
+
+output "instance_profile_name" {
+  value = aws_iam_instance_profile.ec2_profile.name
+}
