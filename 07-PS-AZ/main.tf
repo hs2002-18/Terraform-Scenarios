2317ce74b3fc5fce197f7257c6dad3c905b1b30a
Harsh Shrimali <harshshrimali319@gmail.com>
2026-04-28 05:13:39 +0000

    Created Public Subnet in different AZ's

---
+++ 07-PS-AZ/main.tf
@@ -0,0 +1,36 @@
+provider "aws"{
+    region = "ap-south-1"
+}
+
+resource "aws_vpc" "myvpc"{
+    cidr_block = "10.0.0.0/16"
+    enable_dns_support = true
+    enable_dns_hostnames = true
+
+    tags = {
+        Name = "myVPC"
+    }
+}
+
+resource "aws_subnet" "ps_1"{
+    vpc_id = aws_vpc.myvpc.id
+    cidr_block = "10.0.1.0/24"
+    availability_zone = "ap-south-1a"
+    map_public_ip_on_launch = true
+
+    tags = {
+        Name = "public_subnet_1"
+    }
+}
+
+resource "aws_subnet" "ps_2"{
+    vpc_id = aws_vpc.myvpc.id
+    cidr_block = "10.0.2.0/24"
+    availability_zone = "ap-south-1b"
+    map_public_ip_on_launch = true
+
+    tags = {
+        Name = "public_subnet_2"
+    }
+}
+
